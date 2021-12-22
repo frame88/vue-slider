02a4ce8f3d7718d3945a9ad75bcb466f32f4f1b2
frame88 <fleuzzi97@gmail.com>
2021-12-22 18:54:54 +0100

    previous next

--- js/script.js
+++ js/script.js
@@ -2,46 +2,28 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            counter: null,
+            counter: 0,
             images: [
-                // 'image1.jpg',
-                // 'image2.jpg',
-                // 'image3.jpg',
-                // 'image4.jpg',
-            ]
-        },
-        methods: {
-            next: function () {
-                this.counter += 1;
-                if (this.counter > this.images.length - 1) {
-                    this.counter = 0;
-                }
-            },
-            prev: function () {
-                this.counter -= 1;
-                if (this.counter < 0) {
-                    this.counter = this.images.length - 1;
-                }
-            },
-            alertNextTot: function () {
-                setTimeout(function () {
-                    console.log('timeout');
-
-                }, 1000);
-            }
-        },
-        created() {
-            console.log('created');
-            this.images = [
                 'image1.jpg',
                 'image2.jpg',
                 'image3.jpg',
-                'image4.jpg'
-            ];
+                'image4.jpg',
+            ]
+        },
+        methods: {
+            prv: function () {
+                if (counter > 0) {
+                    this.counter -= 1;
 
-            this.counter = 0;
+                }
+            },
+            nxt: function () {
+                if (counter < 0) {
+                    this.counter += 1;
+    
+                }
+            }
 
-            this.alertNextTot();
         }
     }
 ); 
